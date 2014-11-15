dc0319e20e10c14a43d7daaf1c2154c79f359eb3
Natan Weinberger <naweinberger@gmail.com>
2014-11-15 04:25:49 -0500

    Bathroom list Toolbar text color

--- app/src/main/java/com/palindromicstudios/gottago/MainPage.java
+++ app/src/main/java/com/palindromicstudios/gottago/MainPage.java
@@ -2,6 +2,7 @@ package com.palindromicstudios.gottago;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -16,6 +17,9 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainPage extends ActionBarActivity implements LocationListener {
 
@@ -25,26 +29,34 @@ public class MainPage extends ActionBarActivity implements LocationListener {
     LocationManager mLocationManager;
     private double aLong = 0.0;
     private double aLat = 0.0;
+    public static List<String> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        items = new ArrayList<String>();
+        items.add("Starbucks (Parc/Sherbrooke)");
+        items.add("Second Cup (Parc/Milton");
+        items.add("Schulich Music Building (Sherbrooke/Aylmer)");
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
         setSupportActionBar(toolbar);
+
         ImageButton peeButton = (ImageButton) findViewById(R.id.peeButton);
         peeButton.setOnClickListener( new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                if (hasLocation) {
+                if (hasLocation || 0 == 0) {
                     mLocationManager.removeUpdates(MainPage.this);
                     Intent intent = new Intent(MainPage.this, BathroomList.class);
                     intent.putExtra(USER_LONG, aLong);
                     intent.putExtra(USER_LAT, aLat);
                     startActivity(intent);
+                    overridePendingTransition(R.anim.slide_in_from_right, R.anim.slide_out_to_left);
                 }
                 else {
                     Toast.makeText(MainPage.this, "Please try again later.", Toast.LENGTH_SHORT).show();
@@ -56,10 +68,10 @@ public class MainPage extends ActionBarActivity implements LocationListener {
 
 
         if (mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
         }
         else {
-            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
         }
     }
 
@@ -84,6 +96,7 @@ public class MainPage extends ActionBarActivity implements LocationListener {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
     public void onLocationChanged(Location location) {
         if (location != null) {
             Log.i("Location Changed", location.getLatitude() + " and " + location.getLongitude());
@@ -97,4 +110,29 @@ public class MainPage extends ActionBarActivity implements LocationListener {
     public void onProviderDisabled(String arg0) {}
     public void onProviderEnabled(String arg0) {}
     public void onStatusChanged(String arg0, int arg1, Bundle arg2) {}
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.i("Location changed", "true");
+            hasLocation = true;
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+
 }
